e5ef7c0fe428cb725e5582beae79f9066d5895e9
omp12345 <opsgp1999@gmail.com>
2022-09-20 13:17:06 +0530

    done

---
+++ prime2.js
@@ -0,0 +1,15 @@
+function prime(a){
+let factor=0
+for(i=1;i<=a;i++){
+if(a%2==0){
+factor++
+}
+
+if(factor==2){
+return true
+{
+return false
+}
+let ans=prime(15)
+if(ans==false){
+console.log("Not prime")
